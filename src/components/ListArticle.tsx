bc19598b3441d6ab60d13415f1ff6fa3a26538af
magota <magotadigital@gmail.com>
2025-07-26 10:22:06 +0800

    m

--- src/components/ListArticle.tsx
+++ src/components/ListArticle.tsx
@@ -7,9 +7,9 @@ export function ListArticle(params: any) {
             <p className="font-source text-center font-bold text-2xl lg:text-4xl mb-5 lg:mb-7">
                 {params.blok.headline}
             </p>
-            <p className="mb-7 lg:mb-10 w-full lg:w-1/2 mx-auto text-center text-base lg:text-lg">
+            <div className="mb-7 lg:mb-10 w-full lg:w-1/2 mx-auto text-center text-base lg:text-lg">
                 <StoryblokRichText doc={params.blok.content}/>
-            </p>
+            </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8">
                 {params.blok.articles?.map((article: any) => (
                     <ArticleCard key={article.uuid} article={article} />
